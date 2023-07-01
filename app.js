c1cc197a25dfb5c583a3e897dd3b564a6d1c6cf8
subarthin raja <subarthinraja@gmail.com>
2023-07-01 11:01:50 +0530

    database added

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 var lodash = require('lodash');
+const mongoose=require('mongoose');
 
+mongoose.connect("mongodb://0.0.0.0:27017/blogDB",{useNewUrlParser:true});
+
+const postSchema=mongoose.Schema({
+  name:String,
+  content:String
+})
+
+const Post=mongoose.model("Post",postSchema);
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-let arrobj=[];
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -19,7 +28,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
-  res.render("home",{homecontent:homeStartingContent,postarr:arrobj});
+  Post.find().then(function(data){
+    res.render("home",{homecontent:homeStartingContent,postarr:data});
+  })
+ 
   
 })
 app.get("/about",(req,res)=>{
@@ -32,25 +44,27 @@ app.get("/compose",(req,res)=>{
   res.render("compose");
 })
 app.post("/compose",(req,res)=>{
-    const obj={
-    title:req.body.msgtitle,
+    const post=new Post({
+    name:req.body.msgtitle,
     content:req.body.msgbody
-  };
-  arrobj.push(obj);
+  });
+  post.save();
   res.redirect("/");
 })
 
-app.get("/posts/:postName",(req,res)=>{
-  const pName=req.params.postName;
-  arrobj.forEach(element => {
-    if (lodash.lowerCase(pName)==lodash.lowerCase(element["title"])){
+app.get("/posts/:postId",(req,res)=>{
+  const pName=req.params.postId;
+  Post.findOne({name:pName}).then(function(element){
+    if (lodash.lowerCase(pName)==lodash.lowerCase(element["name"])){
       res.render("post",{posName:pName,posCon:element['content']})
     } 
-  });
+  })
+});
+    
 app.post("/",(req,res)=>{
   //hello
 })
-});
+
 
 
 
